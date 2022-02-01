15815735bce3527ef51acfd8fe6f432a79fc8108
Michele <michelecirillo1999@live.it>
2022-02-01 12:14:11 +0100

    Modificati colori delle regioni

--- contracts/Region.sol
+++ contracts/Region.sol
@@ -25,30 +25,30 @@ contract Region {
         VENETO
     }
 
-    enum Colors {WHITE, YELLOW, ORANGE}
+    enum Colors {WHITE, YELLOW, ORANGE, RED}
 
     mapping (Regions => Colors) public colors;
 
     constructor() {
-        colors[Regions.ABRUZZO] = Colors.WHITE;
+        colors[Regions.ABRUZZO] = Colors.ORANGE;
         colors[Regions.BASILICATA] = Colors.WHITE;
-        colors[Regions.CALABRIA] = Colors.WHITE;
-        colors[Regions.CAMPANIA] = Colors.WHITE;
-        colors[Regions.EMILIA_ROMAGNA] = Colors.WHITE;
-        colors[Regions.FRIULI_VENEZIA_GIULIA] = Colors.WHITE;
-        colors[Regions.LAZIO] = Colors.WHITE;
-        colors[Regions.LIGURIA] = Colors.WHITE;
-        colors[Regions.LOMBARDIA] = Colors.WHITE;
-        colors[Regions.MARCHE] = Colors.WHITE;
+        colors[Regions.CALABRIA] = Colors.YELLOW;
+        colors[Regions.CAMPANIA] = Colors.YELLOW;
+        colors[Regions.EMILIA_ROMAGNA] = Colors.YELLOW;
+        colors[Regions.FRIULI_VENEZIA_GIULIA] = Colors.ORANGE;
+        colors[Regions.LAZIO] = Colors.YELLOW;
+        colors[Regions.LIGURIA] = Colors.YELLOW;
+        colors[Regions.LOMBARDIA] = Colors.YELLOW;
+        colors[Regions.MARCHE] = Colors.YELLOW;
         colors[Regions.MOLISE] = Colors.WHITE;
-        colors[Regions.PIEMONTE] = Colors.WHITE;
-        colors[Regions.PUGLIA] = Colors.WHITE;
-        colors[Regions.SARDEGNA] = Colors.WHITE;
-        colors[Regions.SICILIA] = Colors.WHITE;
-        colors[Regions.TOSCANA] = Colors.WHITE;
-        colors[Regions.TRENTINO_ALTO_ADIGE] = Colors.WHITE;
+        colors[Regions.PIEMONTE] = Colors.ORANGE;
+        colors[Regions.PUGLIA] = Colors.YELLOW;
+        colors[Regions.SARDEGNA] = Colors.YELLOW;
+        colors[Regions.SICILIA] = Colors.ORANGE;
+        colors[Regions.TOSCANA] = Colors.YELLOW;
+        colors[Regions.TRENTINO_ALTO_ADIGE] = Colors.YELLOW;
         colors[Regions.UMBRIA] = Colors.WHITE;
-        colors[Regions.VALLE_DAOSTA] = Colors.WHITE;
-        colors[Regions.VENETO] = Colors.WHITE;
+        colors[Regions.VALLE_DAOSTA] = Colors.ORANGE;
+        colors[Regions.VENETO] = Colors.YELLOW;
     }
 }
