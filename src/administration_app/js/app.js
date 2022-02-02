8f9df74d2115e2e1310c3102a962ebf382cb685a
Michele <michelecirillo1999@live.it>
2022-02-02 16:50:54 +0100

    Finita parte di configurazione, da vedere gli errs

---
+++ src/administration_app/js/app.js
@@ -0,0 +1,71 @@
+let App = {
+  web3Provider: null,
+  contracts: {},
+
+  initWeb3: async function () {
+    // Modern dapp browsers...
+    if (window.ethereum) {
+      App.web3Provider = window.ethereum;
+      try {
+        // Request account access
+        await window.ethereum.request({ method: "eth_requestAccounts" });;
+      } catch (error) {
+        // User denied account access...
+        console.error("User denied account access")
+      }
+    }
+    // Legacy dapp browsers...
+    else if (window.web3) {
+      App.web3Provider = window.web3.currentProvider;
+    }
+    // If no injected web3 instance is detected, fall back to Ganache
+    else {
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+    }
+    web3 = new Web3(App.web3Provider);
+
+    return App.initContract();
+  },
+
+  initContract: function () {
+    $.getJSON('GreenPassCertification.json', function (data) {
+      // Get the necessary contract artifact file and instantiate it with @truffle/contract
+      var GreenPassCertificationArtifact = data;
+      App.contracts.GreenPassCertification = TruffleContract(GreenPassCertificationArtifact);
+
+      // Set the provider for our contract
+      App.contracts.GreenPassCertification.setProvider(App.web3Provider);
+    });
+
+    return true;
+  },
+
+};
+
+
+window.onload = function () {
+  App.initWeb3();
+};
+
+export default function (qrcode) {
+
+  var contractInstance;
+  qrcode.tipo_certificazione = 1; //Provvisorio, perché le stringhe non le vuole
+
+  web3.eth.getAccounts(function (error, accounts) {
+    if (error) {
+      console.log(error);
+    }
+
+    var account = accounts[0];
+
+    App.contracts.GreenPassCertification.deployed().then(function (instance) {
+      contractInstance = instance;
+
+      // Execute adopt as a transaction by sending account
+      return contractInstance.emitCertification(qrcode.tipo_certificazione, JSON.stringify(qrcode), { from: account });
+    }).catch(function (err) {
+      console.log(err.message);
+    });
+  });
+}
--- src/administration_app/js/main.js
+++ src/administration_app/js/main.js
@@ -1,54 +1,62 @@
+import mintCertification from "./app.js";
 
 (function ($) {
     "use strict";
 
+    /*======================================== [Load select values] */
+    loadSelectOptions();
+
     /*==================================================================
     [ Validate after type ]*/
-    $('.validate-input .input100').each(function(){
-        $(this).on('blur', function(){
-            if(validate(this) == false){
+    $('.validate-input .input100').each(function () {
+        $(this).on('blur', function () {
+            if (validate(this) == false) {
                 showValidate(this);
             }
             else {
                 $(this).parent().addClass('true-validate');
             }
-        })    
+        })
     })
-  
-  
+
+
     /*==================================================================
     [ Validate ]*/
     var input = $('.validate-input .input100');
 
-    $('.validate-form').on('submit',function(){
+    $('.validate-form').on('submit', function () {
+        const qrcode = {};
         var check = true;
 
-        for(var i=0; i<input.length; i++) {
-            if(validate(input[i]) == false){
+        for (var i = 0; i < input.length; i++) {
+            if (validate(input[i]) == false) {
                 showValidate(input[i]);
-                check=false;
+                check = false;
             }
+            else
+                qrcode[input[i].name] = input[i].value;
         }
-
+        qrcode.tipo_certificazione = $("#tipo_certificazione")[0].value;
+        mintCertification(qrcode);
         return check;
     });
 
 
-    $('.validate-form .input100').each(function(){
-        $(this).focus(function(){
-           hideValidate(this);
-           $(this).parent().removeClass('true-validate');
+    $('.validate-form .input100').each(function () {
+        $(this).focus(function () {
+            hideValidate(this);
+            $(this).parent().removeClass('true-validate');
         });
     });
 
-     function validate (input) {
-        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
-            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+    function validate(input) {
+        if ($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+            if ($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
                 return false;
             }
         }
         else {
-            if($(input).val().trim() == ''){
+            if ($(input).val().trim() == '') {
                 return false;
             }
         }
@@ -60,9 +68,9 @@
         $(thisAlert).addClass('alert-validate');
 
         $(thisAlert).append('<span class="btn-hide-validate">&#xf136;</span>')
-        $('.btn-hide-validate').each(function(){
-            $(this).on('click',function(){
-               hideValidate(this);
+        $('.btn-hide-validate').each(function () {
+            $(this).on('click', function () {
+                hideValidate(this);
             });
         });
     }
@@ -72,16 +80,42 @@
         $(thisAlert).removeClass('alert-validate');
         $(thisAlert).find('.btn-hide-validate').remove();
     }
-    
+
 
     /*==================================================================
     [ Show / hide contact ]*/
-    $('.btn-hide-contact100').on('click', function(){
+    $('.btn-hide-contact100').on('click', function () {
         $('.container-contact100').fadeOut(300);
     });
 
-    $('.btn-show-contact100').on('click', function(){
+    $('.btn-show-contact100').on('click', function () {
         $('.container-contact100').fadeIn(300);
     });
 
+    // let certificationTypes;
+    // fetch("http://localhost:30303/api/v1/certification/enum/certification_types")
+    //     .then(resp => resp.json())
+    //     .then(data => console.log(data.data))
+    //     .catch(err => alert(err));
+    // console.log(certificationTypes);
+    // for (let i = 0; i < select.length; i++) {
+    //     const option = select[i];
+    //     option.text = "ciao";
+    //     option.value = "ciao";
+    // }
+    // console.log(select);
+
 })(jQuery);
+
+async function loadSelectOptions() {
+    const select = $('#tipo_certificazione');
+    const certificationTypesResp = await fetch("http://localhost:30303/api/v1/certification/enum/certification_types");
+    const certificationTypesJson = await certificationTypesResp.json();
+    const certificationTypes = certificationTypesJson.data;
+    certificationTypes.forEach(type => {
+        const option = document.createElement("option");
+        option.text = type;
+        option.value = type;
+        select.append(option);
+    });
+}
